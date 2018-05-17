097217a8d0651340dbc993aa345b07eae4558078
Rafael Montero <rafaelmonterov@gmail.com>
2018-05-17 16:30:24 -0400

    adding common components like other projects

--- App.js
+++ App.js
@@ -1,58 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import  {Provider, store} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './src/reducers';
+import firebase from 'firebase';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+class App extends Component{
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  componentWillMount(){
+    var config = {
+      apiKey: 'AIzaSyC9TxGjHZC_c2YCmXogl9BFFzr4CwFThSw',
+      authDomain: 'manager-7f7c5.firebaseapp.com',
+      databaseURL: 'https://manager-7f7c5.firebaseio.com',
+      projectId: 'manager-7f7c5',
+      storageBucket: 'manager-7f7c5.appspot.com',
+      messagingSenderId: '1020550055483'
+    };
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    firebase.initializeApp(config);
+  };
+
+  render(){
+    return(
+    <Provider store={createStore(reducers)}>
+      <View>
+          <Text style={styles.landing}>Hello worlds yeah</Text>
+        </View>
+    </Provider>
+    )};
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+const styles = {
+
+  landing:{
+    paddingTop: 40,
+    paddingLeft: 20
+  }
+
+};
+
+export default App;
